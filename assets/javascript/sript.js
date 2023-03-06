05ee575646faa0bcf2a10dbcc7c52dd6bf2dd2cb
carolmedici <carolina.obstetriz@gmail.com>
2023-03-05 23:11:27 -0300

    :lipstick: layout
    Finish desktop layout and add modal in mobile version hamburger menu

--- assets/javascript/sript.js
+++ assets/javascript/sript.js
@@ -1,6 +1,21 @@
 function menuOnClick() {
-    document.getElementById("menu-bar").classList.toggle("change"); // create with toggler the class "change"
-    document.getElementById("nav").classList.toggle("change");
-    document.getElementById("menu-bg").classList.toggle("change-bg");
+     // create with toggler the class "change"
+    document.getElementById("myModal").style.display = "block";  
+   
     //document.getElementById('body').style.backgroundColor = 'black';
 }
+
+
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
+/*
+
+function openModal() {
+    document.getElementById("myModal").style.display = "block";   //change the original "display none" to "block"
+    }
+    
+    // Close the Modal
+    function closeModal() {
+    document.getElementById("myModal").style.display = "none";  //hide again
+    }*/
